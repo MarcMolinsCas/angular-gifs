09216f39a981a7ff5dc32979c5ffdb33afb26ec3
Marc Molins <marcmolins@hotmail.com>
2023-08-06 09:57:36 +0200

    fin sección 7

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,9 @@ export class GifsService {
   private apiKey: string = 'zDr3ykPF6O6DZa7LtGE1MmBwsePr2aXX';
   private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];   //opcional
@@ -26,6 +28,18 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this._tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void {
+    if(!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   searchTag(tag: string): void {
@@ -38,7 +52,11 @@ export class GifsService {
       .set('q', tag);
 
     this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
-      .subscribe( resp  => { this.gifList = resp.data; } );
+      .subscribe( resp  => {
+
+        this.gifList = resp.data;
+
+      } );
 
 
 
